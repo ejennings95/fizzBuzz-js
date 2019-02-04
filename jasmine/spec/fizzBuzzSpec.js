889680b0dcfad31a65fb8a9f4ed0889b492d1fa1
Elliot Jennings <elliot.r.jennings@outlook.com>
2019-02-04 15:54:15 +0000

    first use of jasmine and js - basic tests written

---
+++ jasmine/spec/fizzBuzzSpec.js
@@ -0,0 +1,23 @@
+describe("fizzBuzz", function() {
+  it("it should return 1", function() {
+    expect(fizzBuzz(1)).toEqual(1);
+  });
+});
+
+describe("fizzBuzz", function() {
+  it("it should return fizz", function() {
+    expect(fizzBuzz(3)).toEqual('fizz');
+  });
+});
+
+describe("fizzBuzz", function() {
+  it("it should return buzz", function() {
+    expect(fizzBuzz(5)).toEqual('buzz');
+  });
+});
+
+describe("fizzBuzz", function() {
+  it("it should return fizzbuzz", function() {
+    expect(fizzBuzz(15)).toEqual('fizzbuzz');
+  });
+});
